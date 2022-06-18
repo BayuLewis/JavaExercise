f2f6532417abf3bd576075d5fa01465733a6604a
BayuLewis <bayulewis0312@gmail.com>
2022-06-18 12:19:50 +0700

    Update StringExercise1.java

--- MidTerm1/StringExercise1.java
+++ MidTerm1/StringExercise1.java
@@ -1,3 +1,6 @@
+/* Viết chương trình nhập vào chuỗi Họ và tên, chuyển phần họ của chuỗi họ và tên sang Hoa và các
+ký tự còn lại sang ký tự thường. In kết quả ra màn hình đê kiêm tra.Ví dụ: Nhập vào chuỗi: nGuyEn
+tHI bIch vAN. Kết quả in ra như sau: NGUYEN thi bịch van.*/
 public class StringExercise1{
     static String BitchVanString(String str){
         String[] words = str.split(" ");
